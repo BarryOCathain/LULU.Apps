083c6abb19e65f828a4bec7f9c3939654282b6d6
BarryOCathain <arcangel666@hotmail.co.uk>
2016-06-29 23:05:30 +0100

    Added test method to invok LULU.WCFService and get student.
    Working as expected for Android.

---
+++ LULU.Apps/LULU.Apps/LULU.Apps/App.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace LULU.Apps
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							HorizontalTextAlignment = TextAlignment.Center,
+							Text = "Welcome to Xamarin Forms!"
+						}
+					}
+				}
+			};
+
+            TestGetStudent();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+
+        private void TestGetStudent()
+        {
+            StudentViewModel vm = new StudentViewModel();
+
+            var result = vm.GetStudentByStudentNumber("123456");
+        }
+    }
+}
